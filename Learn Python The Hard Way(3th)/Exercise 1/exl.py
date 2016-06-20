41435decb216dd1136b689eb0bb09b99fd1d3b0e
zero <2541497180@qq.com>
2016-06-20 18:34:04 +0800

    Add Exercise 41

---
+++ Learn Python The Hard Way(3th)/Exercise 1/exl.py
@@ -0,0 +1,9 @@
+# "#"表示注释 
+# 不换行是加","
+print "Hello World!",
+print "Hello Again",
+print "I like typing this.",
+print "This is fun.",
+print "Yay! Printing.",
+print "I'd much rather you 'not'.",
+print 'I "said" do not touch this.',
